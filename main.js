62bccf91aadf89b003a0a3ae13c176f89c9ad0e1
Karen Montes <karen.montes04@gmail.com>
2019-12-30 21:48:15 -0700

    added ajax.html to test ajax call because I realized I need a third ajax call for the uv index

--- main.js
+++ main.js
@@ -1,42 +1,42 @@
-const citySearch = $("#searchbox")
-    .val()
-    .trim();
+const citySearch = $(".searchbox").val();
 
-
-
-
-$.ajax({
-    url: "https://api.openweathermap.org/data/2.5/weather?q=Phoenix&appid=5fc03cb1215e62a9fc90d5f3f388336f",
-    method: "GET"
-}).then(function (response) {
-
-    console.log("city:", response.name)
-    console.log("Date:", moment(response.dt, "X").format("l"))
-    console.log("temperature:", ((response.main.temp - 273.15) * 1.8) + 32)
+$("#search-icon").click(function() {
     $.ajax({
-        url: "http://api.openweathermap.org/data/2.5/forecast?q=" + response.name + "&appid=5fc03cb1215e62a9fc90d5f3f388336f",
+        url: "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&appid=5fc03cb1215e62a9fc90d5f3f388336f",
         method: "GET"
-    })
-        .then(function (response) {
-            // console.log(response)
-            var previousDate = "";
-           // previousDate = moment(response.list[0].dt, "X").format("l")
-                
-            for (let i = 0; i < response.list.length; i++) {
-                var current = moment(response.list[i].dt, "X").format("l");
-                if ( current != previousDate) {
-                    console.log("Date:", moment(response.list[i].dt, "X").format("l"))
-                    console.log("temperature:", ((response.list[i].main.temp - 273.15) * 1.8) + 32)
-                  
-                }
-                 
-                
-                previousDate = moment(response.list[i].dt, "X").format("l")
-                
+    }).then(function (response) {
 
-               
-            }
-            console.log()
+        const city1 = response.name
+        const cityName = $("#city-title")
+        cityName.text(city1)
+        
+        console.log("Date:", moment(response.dt, "X").format("l"))
+        console.log("temperature:", ((response.main.temp - 273.15) * 1.8) + 32)
+        $.ajax({
+            url: "http://api.openweathermap.org/data/2.5/forecast?q=" + response.name + "&appid=5fc03cb1215e62a9fc90d5f3f388336f",
+            method: "GET"
         })
+            .then(function (response) {
+                // console.log(response)
+                var previousDate = "";
+                // previousDate = moment(response.list[0].dt, "X").format("l")
 
-})
+                for (let i = 0; i < response.list.length; i++) {
+                    var current = moment(response.list[i].dt, "X").format("l");
+                    if (current != previousDate) {
+                        console.log("Date:", moment(response.list[i].dt, "X").format("l"))
+                        console.log("temperature:", ((response.list[i].main.temp - 273.15) * 1.8) + 32)
+
+                    }
+
+
+                    previousDate = moment(response.list[i].dt, "X").format("l")
+
+
+
+                }
+                console.log()
+            })
+
+    })
+});
