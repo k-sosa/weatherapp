16c9c1067b8ef8ffe93fd0843beb6a5ac660cb72
Karen Montes <karen.montes04@gmail.com>
2020-01-01 22:45:59 -0700

    added fahrenheit symbol to temps

--- main.js
+++ main.js
@@ -2,41 +2,37 @@
 let searchArray = []
 let citySearch = $(".searchbox").val();
 saveSearch(citySearch)
-function saveSearch( citySearch){
-     let storage = localStorage.getItem("savedSearches")
-          if(storage === null && citySearch.length>0){
-            searchArray.push(citySearch)
-            localStorage.setItem("savedSearches",JSON.stringify(searchArray))//stringify converts array to string
-          }
-          else {
-              searchArray = JSON.parse(storage)//parse converts string to array
-              searchArray.push(citySearch)
-          }
-          $(".previous-search").empty()
-         for (let index = 0; index < searchArray.length; index++) {
-             
-               if(searchArray[index].length>0){
-                let button = $("<button>")
-                button.text(searchArray[index])
-                $(".previous-search").append(button,"<br>")
-                localStorage.setItem("savedSearches",JSON.stringify(searchArray))//stringify converts array to string
-               }
-               
-         }
+function saveSearch(citySearch) {
+    let storage = localStorage.getItem("savedSearches")
+    if (storage === null && citySearch.length > 0) {
+        searchArray.push(citySearch)
+        localStorage.setItem("savedSearches", JSON.stringify(searchArray))//stringify converts array to string
+    }
+    else {
+        searchArray = JSON.parse(storage)//parse converts string to array
+        searchArray.push(citySearch)
+    }
+    $(".previous-search").empty()
+    for (let index = 0; index < searchArray.length; index++) {
+
+        if (searchArray[index].length > 0) {
+            let button = $("<button>")
+            button.text(searchArray[index])
+            $(".previous-search").append(button, "<br>")
+            localStorage.setItem("savedSearches", JSON.stringify(searchArray))//stringify converts array to string
+        }
+
+    }
 
 }
 
 
-$("#search-icon").on("click",function () {
-     citySearch = $(".searchbox").val();
+$("#search-icon").on("click", function () {
+    citySearch = $(".searchbox").val();
 
 
     saveSearch(citySearch)
- 
 
-
-
-   
     $.ajax({
         url: "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&appid=5fc03cb1215e62a9fc90d5f3f388336f",
         method: "GET"
@@ -46,7 +42,7 @@ $("#search-icon").on("click",function () {
         const cityName = $("#city-title")
         cityName.text(city1)
 
-        
+
 
         console.log("Date:", moment(response.dt, "X").format("l"))
         let temp = ((response.main.temp - 273.15) * 1.8) + 32
@@ -57,10 +53,11 @@ $("#search-icon").on("click",function () {
         $("#humidity").text(humidity1)
         const windSpeed = response.wind.speed
         $("#wind-speed").text(windSpeed)
-        const iconcode = response.weather.icon //not sure why this is greyed out
+        const iconcode = response.weather.icon
+        const iconUrl = "http://openweathermap.org/img/w/" + iconcode + ".png" //not sure why it is grayed out here
         const longitude = response.coord.lon
         const latitude = response.coord.lat
-        
+
         $.ajax({
             url: "http://api.openweathermap.org/data/2.5/forecast?q=" + response.name + "&appid=5fc03cb1215e62a9fc90d5f3f388336f",
             method: "GET"
@@ -69,27 +66,27 @@ $("#search-icon").on("click",function () {
                 // console.log(response)
                 var previousDate = "";
                 // previousDate = moment(response.list[0].dt, "X").format("l")
-                 let count = 0  
+                let count = 0
                 for (let i = 0; i < response.list.length; i++) {
                     var current = moment(response.list[i].dt, "X").format("l");
                     if (current != previousDate) {
                         let day = moment(response.list[i].dt, "X").format("l")
-                        console.log("Date:", day )
-                        $("#date"+count).text(day)
-                        
+                        console.log("Date:", day)
+                        $("#date" + count).text(day)
+
                         let temp = ((response.list[i].main.temp - 273.15) * 1.8) + 32
                         temp = Math.round(temp)
-                        $("#temp"+count).text(temp)
+                        $("#temp" + count).text(temp)
 
                         let humidity = response.list[i].main.humidity
-                        $("#humidity"+count).text(humidity)
+                        $("#humidity" + count).text(humidity)
 
                         let iconcode = response.list[i].weather.icon
                         const iconUrl = "http://openweathermap.org/img/w/" + iconcode + ".png"
-                        $('#wicon'+count).attr('src', iconUrl)
+                        $('#wicon' + count).attr('src', iconUrl)
 
 
-                          count++
+                        count++
                     }
 
 
